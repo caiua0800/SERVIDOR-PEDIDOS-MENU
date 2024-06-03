f71c52aa361c3beee90563817e479bbf0c12b63a
Dev Mello <caiuabrandao@gmail.com>
2024-06-03 12:37:08 -0300

    First Commit

---
+++ SERVER.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let listaPedidos = []; 
+let listaProntos = []; 
+
+app.post('/addPedidos', (req, res) => {
+    const data = req.body;
+    console.log('Dados recebidos:', data);
+
+    listaPedidos.push(data);
+
+    res.send('POST recebido com sucesso!');
+});
+
+app.get('/getPedidos', (req, res) => {
+    res.json(listaPedidos);
+});
+
+app.get('/getProntos', (req, res) => {
+    res.json(listaProntos);
+});
+
+app.post('/pedidoPronto', (req, res) => {
+    const data = req.body;
+    console.log('Pedido pronto:', data);
+
+    const index = listaPedidos.findIndex(pedido => pedido.nome === data.nome && pedido.mesa === data.mesa);
+    
+    if (index !== -1) {
+        listaPedidos.splice(index, 1);
+        listaProntos.push(data);
+
+        res.send('Pedido marcado como pronto com sucesso!');
+    } else {
+        res.status(404).send('Pedido não encontrado na lista de pedidos.');
+    }
+});
+
+
+
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) });
